cae0870824787839aeba889b9dde2d67d8bbabbd
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-10 22:36:23 +0100

    Move tests to tests package

--- test/example_test/example_test.sh
+++ test/example_test/example_test.sh
@@ -4,22 +4,22 @@ set -euo pipefail
 
 script_directory="$(dirname "$0")"
 
-if ! "$script_directory/../01_simple_example.sh"; then
+if ! "$script_directory/../../example/01_simple_example.sh"; then
   echo "Example should pass but fails."
   exit 1
 fi
 
-if "$script_directory/../02_failing_example.sh"; then
+if "$script_directory/../../example/02_failing_example.sh"; then
   echo "Example should fail but passes."
   exit 1
 fi
 
-if ! "$script_directory/../03_example_with_specific_expectation.sh"; then
+if ! "$script_directory/../../example/03_example_with_specific_expectation.sh"; then
   echo "Example should pass but fails."
   exit 1
 fi
 
-if "$script_directory/../04_nested_example.sh"; then
+if "$script_directory/../../example/04_nested_example.sh"; then
   echo "Example should fail but passes."
   exit 1
 fi
