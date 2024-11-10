ec1c031d2d15c3ef50ade25f69faf5cb5fa6858e
anton <57831993+antonrotar@users.noreply.github.com>
2024-11-10 22:18:58 +0100

    Merge example into main repo

---
+++ example/02_failing_example.sh
@@ -0,0 +1,12 @@
+set -euo pipefail
+
+# This example adds and runs one failing and one passing command.
+# The overall return value will be negative.
+
+script_directory="$(dirname "$0")"
+source "$script_directory/../command_runner.sh"
+
+command_runner_add ./$script_directory/commands/failing_command.sh
+command_runner_add ./$script_directory/commands/passing_command.sh
+
+command_runner_run
---
+++ example/04_nested_example.sh
@@ -0,0 +1,13 @@
+set -euo pipefail
+
+# This example adds and runs multiple examples. One of them will fail.
+# The overall return value will be negative.
+
+script_directory="$(dirname "$0")"
+source "$script_directory/../command_runner.sh"
+
+command_runner_add ./$script_directory/01_simple_example.sh
+command_runner_add ./$script_directory/02_failing_example.sh
+command_runner_add_with_expectation ./$script_directory/02_failing_example.sh 1
+
+command_runner_run
