219381233f1130db875f4fe0ab2c767e75cf390f
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-09 22:26:31 +0100

    Use more robust method for script directory

--- examples/01_simple_example.sh
+++ examples/01_simple_example.sh
@@ -3,7 +3,7 @@
 # This example adds and runs two passing commands.
 # The overall return value will be positive.
 
-SCRIPT_DIRECTORY="$(dirname "$0")"
+SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
 source "$SCRIPT_DIRECTORY/../command_runner.sh"
 
 command_runner_add ./$SCRIPT_DIRECTORY/commands/passing_command.sh
