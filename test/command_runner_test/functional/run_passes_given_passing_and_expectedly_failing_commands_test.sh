4a86b5586809aa72e4f30335829b5e993f8aa14f
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-09 15:12:47 +0100

    Implement functional tests

---
+++ test/command_runner_test/functional/run_passes_given_passing_and_expectedly_failing_commands_test.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -u
+
+SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIRECTORY/../support/support.sh"
+
+source_command_runner
+
+command_runner_add passing_command
+command_runner_add_with_expectation failing_command 1
+
+OUTPUT=$(command_runner_run)
+
+expect_success $?
+expect_log_contains "$(extract_logs "$OUTPUT")" "passing_command 0\nfailing_command 1"
+expect_log_empty "$(extract_errors "$OUTPUT")"
+expect_log_contains "$(extract_results "$OUTPUT")" "passing_command 0 PASSED\nfailing_command 1 PASSED"
---
+++ test/command_runner_test/functional/run_passes_given_passing_commands_test.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -u
+
+SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIRECTORY/../support/support.sh"
+
+source_command_runner
+
+command_runner_add passing_command
+command_runner_add another_passing_command
+
+OUTPUT=$(command_runner_run)
+
+expect_success $?
+expect_log_contains "$(extract_logs "$OUTPUT")" "passing_command 0\nanother_passing_command 0"
+expect_log_empty "$(extract_errors "$OUTPUT")"
+expect_log_contains "$(extract_results "$OUTPUT")" "passing_command 0 PASSED\nanother_passing_command 0 PASSED"
