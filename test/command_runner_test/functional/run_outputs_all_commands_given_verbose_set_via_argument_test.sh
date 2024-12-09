1956eb6ffa67c95f6b1636ac77f28c629968ef6d
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-09 15:12:47 +0100

    Implement functional tests

---
+++ test/command_runner_test/functional/run_outputs_all_commands_given_verbose_set_via_argument_test.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -u
+
+SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIRECTORY/../support/support.sh"
+
+source_command_runner
+
+command_runner_add passing_command
+command_runner_add failing_command
+
+OUTPUT=$(command_runner_run "-v")
+
+expect_failure $?
+expect_log_contains "$(extract_logs "$OUTPUT")" "passing_command 0\nOutput from passing command\nfailing_command 0\nOutput from failing command"
+expect_log_contains "$(extract_errors "$OUTPUT")" "Error executing: failing_command 0"
+expect_log_contains "$(extract_errors "$OUTPUT")" "Output from failing command"
+expect_log_contains "$(extract_results "$OUTPUT")" "passing_command 0 PASSED\nfailing_command 0 FAILED"
