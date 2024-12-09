a0187f3f41788315fcf147d4f31799266d7a7c5c
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-09 16:05:53 +0100

    Use test support to simplify test

--- test/examples/examples_test.sh
+++ test/examples/examples_test.sh
@@ -1,27 +1,16 @@
-set -euo pipefail
+#! /bin/bash -u
 
-# This test verifies the assumptions from the example execution.
+SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIRECTORY/../support/support.sh"
 
-script_directory="$(dirname "$0")"
+"$SCRIPT_DIRECTORY/../../examples/01_simple_example.sh"
+expect_success $?
 
-if ! "$script_directory/../../examples/01_simple_example.sh"; then
-  echo "Example should pass but fails."
-  exit 1
-fi
+"$SCRIPT_DIRECTORY/../../examples/02_failing_example.sh"
+expect_failure $?
 
-if "$script_directory/../../examples/02_failing_example.sh"; then
-  echo "Example should fail but passes."
-  exit 1
-fi
+"$SCRIPT_DIRECTORY/../../examples/03_example_with_specific_expectation.sh"
+expect_success $?
 
-if ! "$script_directory/../../examples/03_example_with_specific_expectation.sh"; then
-  echo "Example should pass but fails."
-  exit 1
-fi
-
-if "$script_directory/../../examples/04_nested_example.sh"; then
-  echo "Example should fail but passes."
-  exit 1
-fi
-
-echo "All tests passed."
+"$SCRIPT_DIRECTORY/../../examples/04_nested_example.sh"
+expect_failure $?
