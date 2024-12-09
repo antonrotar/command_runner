fd5b63be551ef864dfd26afd030333bc99f19006
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-09 13:03:24 +0100

    Assert on specific logs for run command

--- test/command_runner_test/contract/run_passes_given_streamed_argument_test.sh
+++ test/command_runner_test/contract/run_passes_given_streamed_argument_test.sh
@@ -8,3 +8,6 @@ source_command_runner
 OUTPUT=$(command_runner_run "-s")
 
 expect_success $?
+expect_log_empty "$(extract_logs "$OUTPUT")"
+expect_log_empty "$(extract_errors "$OUTPUT")"
+expect_log_empty "$(extract_results "$OUTPUT")"
