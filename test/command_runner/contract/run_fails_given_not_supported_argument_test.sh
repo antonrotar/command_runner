6c561d333ddba0998b8ea822093520ef3f9860ab
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-09 15:24:55 +0100

    Move support package one layer up

--- test/command_runner/contract/run_fails_given_not_supported_argument_test.sh
+++ test/command_runner/contract/run_fails_given_not_supported_argument_test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -u
 
 SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
-source "$SCRIPT_DIRECTORY/../support/support.sh"
+source "$SCRIPT_DIRECTORY/../../support/support.sh"
 
 source_command_runner
 
