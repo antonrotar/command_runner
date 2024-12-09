ab8ce259801af2d381663d1419827f8287ad6604
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-09 22:26:31 +0100

    Use more robust method for script directory

--- examples/03_example_with_specific_expectation.sh
+++ examples/03_example_with_specific_expectation.sh
@@ -3,7 +3,7 @@
 # This example adds and runs one failing command with specific expectation.
 # The overall return value will be positive.
 
-SCRIPT_DIRECTORY="$(dirname "$0")"
+SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
 source "$SCRIPT_DIRECTORY/../command_runner.sh"
 
 command_runner_add_with_expectation ./$SCRIPT_DIRECTORY/commands/failing_command.sh 1
--- examples/04_nested_example.sh
+++ examples/04_nested_example.sh
@@ -3,7 +3,7 @@
 # This example adds and runs multiple examples. One of them will fail.
 # The overall return value will be negative.
 
-SCRIPT_DIRECTORY="$(dirname "$0")"
+SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
 source "$SCRIPT_DIRECTORY/../command_runner.sh"
 
 command_runner_add ./$SCRIPT_DIRECTORY/01_simple_example.sh
