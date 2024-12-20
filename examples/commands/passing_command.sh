8d8e00ed0e7218bdc5dd8a0eacb7b47840b0ea91
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-21 00:52:09 +0100

    Simplify examples

--- examples/commands/passing_command.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -u
-
-# Basic script returning a positive value as a substitute
-# for whatever command the command runner will be executing.
-
-echo "This is a passing command."
-
-exit 0
