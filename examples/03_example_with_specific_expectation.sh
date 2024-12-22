0ebcb83266a7d9deaf606cc562402fdc29b56378
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-22 23:29:10 +0100

    Improve documentation in example scripts

--- examples/03_example_with_specific_expectation.sh
+++ examples/03_example_with_specific_expectation.sh
@@ -1,11 +1,17 @@
 #! /bin/bash -u
 
-# This example adds and runs one failing command with specific expectation.
-# The overall return value will be positive.
-
+# Import command runner script.
 SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
 source "$SCRIPT_DIRECTORY/../command_runner.sh"
 
+# Add a failing command but expect it to fail so it will be evaluated as "passed".
+# Note: /bin/false will do nothing but return a status code indicating failure.
+# Check "man /bin/false" for details.
 command_runner_add_with_expectation "echo Output from failing command;/bin/false" 1
 
+# Run commands. The overall result will be positive.
+# Pass "$@" to the run function. It allows to run the whole
+# example script like
+# ./03_example_with_specific_expectation.sh -v
+# to change output options and observe the results.
 command_runner_run "$@"
