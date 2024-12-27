ee8212f3443f93a05e0a81ef94b693b32bce8c61
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-27 09:40:50 +0100

    Add missing explicit test for default behavior

---
+++ test/command_runner/functional/run_outputs_only_failed_commands_per_default_test.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -u
+
+SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIRECTORY/../../support/support.sh"
+
+source_command_runner
+
+command_runner_add passing_command
+command_runner_add failing_command
+
+OUTPUT=$(command_runner_run)
+
+expect_failure $?
+expect_log_contains "$(extract_logs "$OUTPUT")" "passing_command\nfailing_command"
+expect_log_contains "$(extract_errors "$OUTPUT")" "failing_command"
+expect_log_contains "$(extract_errors "$OUTPUT")" "Output from failing command"
+expect_log_contains "$(extract_results "$OUTPUT")" "passing_command PASSED\nfailing_command FAILED"
