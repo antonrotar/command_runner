ec0d66b936a432b3625c97da9f1d94690ed181e3
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-28 08:59:16 +0100

    Prevent log spamming in tests

--- test/examples/examples_test.sh
+++ test/examples/examples_test.sh
@@ -3,14 +3,14 @@
 SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
 source "$SCRIPT_DIRECTORY/../support/support.sh"
 
-"$SCRIPT_DIRECTORY/../../examples/01_simple_example.sh"
+"$SCRIPT_DIRECTORY/../../examples/01_simple_example.sh" >/dev/null
 expect_success $?
 
-"$SCRIPT_DIRECTORY/../../examples/02_failing_example.sh"
+"$SCRIPT_DIRECTORY/../../examples/02_failing_example.sh" >/dev/null
 expect_failure $?
 
-"$SCRIPT_DIRECTORY/../../examples/03_example_with_specific_expectation.sh"
+"$SCRIPT_DIRECTORY/../../examples/03_example_with_specific_expectation.sh" >/dev/null
 expect_success $?
 
-"$SCRIPT_DIRECTORY/../../examples/04_nested_example.sh"
+"$SCRIPT_DIRECTORY/../../examples/04_nested_example.sh" >/dev/null
 expect_failure $?
