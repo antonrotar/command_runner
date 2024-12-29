878ecdc7aa95a12299ebee4d7b3562b773a66238
anton <57831993+antonrotar@users.noreply.github.com>
2024-12-29 09:02:20 +0100

    Fix documentation

--- examples/05_stop_on_failure_example.sh
+++ examples/05_stop_on_failure_example.sh
@@ -4,8 +4,13 @@
 SCRIPT_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
 source "$SCRIPT_DIRECTORY/../command_runner.sh"
 
+# Configure the command runner to stop on failure.
+# Per default it will continue on failure.
 command_runner_stop_on_failure
 
+# Add some passing and some failing commands.
+# The first failing command will stop the execution of all subsequent commands.
+# Run with "-v" to observe the behavior.
 command_runner_add "echo Output from passing command"
 command_runner_add "echo Output from failing command;/bin/false"
 command_runner_add "echo Output from another failing command;/bin/false"
@@ -14,6 +19,6 @@ command_runner_add "echo Output from another passing command"
 # Run commands. The overall result will be negative.
 # Pass "$@" to the run function. It allows to run the whole
 # example script like
-# ./04_nested_example.sh -v
+# ./05_stop_on_failure_example.sh -v
 # to change output options and observe the results.
 command_runner_run "$@"
