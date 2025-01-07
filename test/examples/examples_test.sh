73281e281bae631fca9f13bf6f957831c71c9e22
anton <57831993+antonrotar@users.noreply.github.com>
2025-01-07 12:54:53 +0100

    Ensure all examples have tests

--- test/examples/examples_test.sh
+++ test/examples/examples_test.sh
@@ -14,3 +14,14 @@ expect_success $?
 
 "$SCRIPT_DIRECTORY/../../examples/04_nested_example.sh" >/dev/null
 expect_failure $?
+
+"$SCRIPT_DIRECTORY/../../examples/05_stop_on_failure_example.sh" >/dev/null
+expect_failure $?
+
+# Simple smoke test that all examples have a corresponding test.
+NUMBER_OF_TESTS=5
+NUMBER_OF_EXAMPLES=$(ls $SCRIPT_DIRECTORY/../../examples/*.sh | wc -l)
+if [ "$NUMBER_OF_TESTS" -ne "$NUMBER_OF_EXAMPLES" ]; then
+  echo "Number of tests ($NUMBER_OF_TESTS) does not match number of examples ($NUMBER_OF_EXAMPLES)."
+  exit 1
+fi
